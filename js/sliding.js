c78804451b744371e3648a79cb47b84d2ce323c7
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-02 01:37:17 -0700

    minWindowInputs finished

--- js/sliding.js
+++ js/sliding.js
@@ -1,4 +1,4 @@
-var findMaxInputs = [
+let findMaxInputs = [
     [[1,2,3,4,5,6,7,8,9,10], 3],
     [[3,3,3,3,3,3,3,3,3,3], 4],
     [[10,6,9,-3,23,-1,34,56,67,-1,-4,-8,-2,9,10,34,67], 2],
@@ -27,10 +27,55 @@ function findMaxSlidingWindow(nums, w) {
     return results;
 }
 
-for (let set of findMaxInputs){
+/*for (let set of findMaxInputs){
     cc(findMaxSlidingWindow(set[0], set[1]));
-}
+}*/
 
 
 //--------------------------------------------------//
 
+
+let minWindowInputs = [
+    ["abcdebdde", "bde"],
+    ["fgrqsqsnodwmxzkzxwqegkndaa", "kzed"],
+    ["michmznaitnjdnjkdsnmichmznait", "michmznait"],
+    ["afgegrwgwga", "aa"],
+    ["abababa", "ba"],
+];
+
+function minWindow(str1, str2) {
+    let workingWindow = [];
+    let result = [];
+    let entryCount = str2.length;
+    let workingOnEntryNumber = 0;
+    let goalAchieved = false;
+
+    let splitStr1 = str1.split("");
+    let splitStr2 = str2.split("");
+
+    outer:
+    for (let entry of splitStr1){
+        if (workingOnEntryNumber > entryCount-1) {
+            break outer;
+        }
+
+        if (result[0]) workingWindow.push(entry);
+
+        if (entry === splitStr2[workingOnEntryNumber]){
+            if (result[0]) {
+                result = workingWindow;
+            } else {
+                result[0] = entry;
+                workingWindow.push(entry);
+            }
+            workingOnEntryNumber++;
+        }
+
+    }
+
+    return (goalAchieved ? "" : result.join(""));
+}
+
+for (let entry of minWindowInputs){
+    cc(minWindow(entry[0], entry[1]));
+}

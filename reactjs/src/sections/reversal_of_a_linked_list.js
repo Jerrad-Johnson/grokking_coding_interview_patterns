4f1fc2b58621ac9d6eb96cbe09f6e74fbc330aa3
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-08 01:14:16 -0700

    Finished reverseLinkedList

--- reactjs/src/sections/reversal_of_a_linked_list.js
+++ reactjs/src/sections/reversal_of_a_linked_list.js
@@ -5,7 +5,11 @@ export function reversalOfALinkedList(){
     let headToBeReversed = new LinkedList();
     //headToBeReversed.createLinkedList([1,-2,3,4,-5,4,3,-2,1]);
     headToBeReversed.createLinkedList([1,5,6,7,8]);
-    reverse(headToBeReversed.head);
+    //reverse(headToBeReversed.head);
+
+    let linkedHeadToBeReversed = new LinkedList();
+    linkedHeadToBeReversed.createLinkedList([3,4,5,6,2,8,7,7]);
+    reverseLinkedList(linkedHeadToBeReversed.head, 3);
 }
 
 
@@ -30,3 +34,32 @@ function reverse(head) {
 
     return reversedList;
 }
+
+
+
+//--------------------------------------------------//
+// I had no clue how to do this, I stole the code from Google and then spent time with it.
+//--------------------------------------------------//
+
+function reverseLinkedList(head, k){
+    let n = 0;
+    for (let i = head; i !== null; n++, i = i.next);
+
+    let dummy = new LinkedList();
+    dummy.next = head;
+
+    for (let newHead = dummy, tail = head; n >= k; n -= k){
+        for (let i = 1; i < k; i++){
+            let next = tail.next.next;
+            tail.next.next = newHead.next;
+            newHead.next = tail.next;
+            tail.next = next;
+        }
+        newHead = tail;
+        tail = tail.next;
+    }
+
+    cc(dummy.next);
+}
+
+
